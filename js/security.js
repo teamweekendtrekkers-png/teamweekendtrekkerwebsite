9f6582ef2940c08f5f0a439863501e806d56d8a6
teamweekendtrekkers-png <teamweekendtrekkers@gmail.com>
2026-01-03 16:42:07 +0530

    Update UPI ID via Trip Manager App

--- js/security.js
+++ js/security.js
@@ -18,10 +18,10 @@
     
     const SecurityConfig = {
         // UPI stored as ASCII character codes (not plain text)
-        _p1: [57,53,51,56,50,51,54,53,56,49], 
+        _p1: [112,97,121,116,109,113,114,53,110,98,56,49,116], 
         _p2: 64,  
-        _p3: [121,98,108], 
-        _checksum: 1165100733,
+        _p3: [112,116,121,115], 
+        _checksum: -167222262,
         
         _rateLimits: {
             copyAttempts: { max: 5, window: 60000 },
